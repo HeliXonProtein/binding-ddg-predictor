1d44305220642579dc25deeffbfb5f9f901b18bd
Sven Giese <6873303+gieses@users.noreply.github.com>
2023-06-13 14:55:21 +0200

    restructure package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ddg_predictor',
+    version='1.0.0',
+    url='https://github.com/HeliXonProtein/binding-ddg-predictor',
+    author='Author Name',
+    author_email='luost@helixon.com',
+    description="""ddg-predictor predicts changes in binding energy upon mutation (ddG) for protein-protein complexes.""",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    package_data={'ddg_predictor': ['data/model.pt']},
+    entry_points={
+        'console_scripts': [
+            'ddg_predict = ddg_predictor.scripts.predict:__main__',
+        ],
+    },
+)
